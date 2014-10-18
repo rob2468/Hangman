f177c484d3febf9cd7fab4b313659de54fa3fc3a
jam <jam.chenjun@gmail.com>
2014-10-18 21:08:05 +0800

    add progress view

---
+++ Hangman/HMProgressView.h
@@ -0,0 +1,13 @@
+//
+//  HMProgressView.h
+//  Hangman
+//
+//  Created by junchen on 10/18/14.
+//  Copyright (c) 2014 Strikingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMProgressView : UIView
+
+@end
---
+++ Hangman/HMProgressView.m
@@ -0,0 +1,21 @@
+//
+//  HMProgressView.m
+//  Hangman
+//
+//  Created by junchen on 10/18/14.
+//  Copyright (c) 2014 Strikingly. All rights reserved.
+//
+
+#import "HMProgressView.h"
+
+@implementation HMProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
